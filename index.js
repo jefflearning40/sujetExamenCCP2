eb0c39c6bc56675a08234a7cc01b8809821de981
jefflearning40 <jfld791000@gmail.com>
2025-09-11 12:28:30 +0200

    index.js in work

--- index.js
+++ index.js
@@ -2,16 +2,29 @@ import express from "express";
 import router from "./routes/user.routes.js";
 import userRouter from "./routes/mission.route.js";
 
+const bcrypt = require('bcrypt');
 const app = express();
 
 app.use(express.json());
 
-app.use("/hello", router);
+app.use("/benevole", router);
 
-app.use("/users", userRouter);
+app.use("/mission", userRouter);
+
+//--------------------------------------------------------------le JWT-------------------------------------------------
+const PasswordAdmin="adminasso";
+const saltRounds=10;
+bcrypt.hash(PasswordAdmin,saltRounds,(err,hash)=>{
+    if(err){
+        console.error(err);
+        console.log('mdp invalide');
+    }else{
+        console.log(hash);
+    }   
+});
 
 
 app.listen(3000, () => {
-  console.log("server demarré sur ::: http://localhost:3000");
+  console.log("server MTR launched in ::: http://localhost:3000");
   
 });
