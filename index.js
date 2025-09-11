633f7b13e71609560f063095db83f1bfd6953a67
jefflearning40 <jfld791000@gmail.com>
2025-09-11 13:29:56 +0200

    test bcrypte

--- index.js
+++ index.js
@@ -12,16 +12,25 @@ app.use("/benevole", router);
 app.use("/mission", userRouter);
 
 //--------------------------------------------------------------le JWT-------------------------------------------------
-const PasswordAdmin="adminasso";
-const saltRounds=10;
-bcrypt.hash(PasswordAdmin,saltRounds,(err,hash)=>{
-    if(err){
-        console.error(err);
-        console.log('mdp invalide');
-    }else{
-        console.log(hash);
-    }   
-});
+// Mot de passe en clair (à hacher)
+const PasswordAdminAsso = 'adminasso';
+
+// Nombre de tour de salt, ici 2 puissance 10 passages a executer l'algorythme mais c'est plus lent mais plus cher pour l'attaquant
+const saltRounds = 10;  
+
+async function run() {
+  try {
+    // Génère le hash du mot de passe
+    const hashedPassword = await bcrypt.hash(PAsswordAdminAsso, saltRounds);
+    console.log('mot de pass hashed:', hashedPassword);
+
+    // Vérifie ensuite le mot de passe
+    const isMatch = await bcrypt.compare(plainPassword, hashedPassword);
+    console.log('Mot de passe correct ?', isMatch);
+  } catch (err) {
+    console.error('Erreur bcrypt:', err);
+  }
+}
 
 
 app.listen(3000, () => {
